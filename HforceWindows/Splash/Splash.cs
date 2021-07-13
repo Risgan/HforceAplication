12c1173ef1d6e7e5f97f1b09729114c26d58dda6
JOHN ALVARO RUEDA FORERO <risgan2727@gmail.com>
2021-07-13 12:22:18 -0500

    Se agrega formulario Splash

--- HforceWindows/Splash/Splash.cs
+++ HforceWindows/Splash/Splash.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HforceWindows
+namespace HforceWindows.Splash
 {
-    public partial class Form1 : Form
+    public partial class Splash : Form
     {
-        public Form1()
+        public Splash()
         {
             InitializeComponent();
         }
